fabd9bd784928c7446b58c48d6254e75805260a6
synqotik <stheleman@opennms.com>
2022-09-22 14:03:37 -0400

    Small fixes to config files

--- run-mos-cdr-processor.sh
+++ run-mos-cdr-processor.sh
@@ -3,5 +3,6 @@
 # run from target directory`
 # Put your actual runConfig.json inside data (may contain sensitive data)
 # The sample one is in the base directory
-java -jar moscdrprocessor-0.0.9-SNAPSHOT-onejar.jar process -v \
+cd target
+java -jar moscdrprocessor-0.0.1-SNAPSHOT-onejar.jar folder -v \
     --config "~/projects/mos-cdr-processor/data/runConfig.json"
